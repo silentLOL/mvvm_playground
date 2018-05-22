eac11b7170a7603d534f97029b4751a4089bc3ac
Stefan Irndorfer <stefanirndorfer@Stefans-MacBook-Pro-2.local>
2018-05-22 08:24:18 +0200

    Greetings Dialog added.

--- app/src/main/java/stefanirndorfer/at/mvvm_playground/view/MainActivity.java
+++ app/src/main/java/stefanirndorfer/at/mvvm_playground/view/MainActivity.java
@@ -1,13 +1,17 @@
 package stefanirndorfer.at.mvvm_playground.view;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import stefanirndorfer.at.mvvm_playground.R;
+import stefanirndorfer.at.mvvm_playground.databinding.ActivityMainBinding;
+import stefanirndorfer.at.mvvm_playground.viewmodel.ActivityMainViewModel;
 
 public class MainActivity extends AppCompatActivity implements GreetingDialogListener {
 
     private static final String GREETING_DIALOG_TAG = "greeting_dialog_tag";
+    //private ActivityMainViewModel activityMainViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,11 @@ public class MainActivity extends AppCompatActivity implements GreetingDialogLis
 
     @Override
     public void onDialogDone() {
+        initDataBinding();
+    }
 
+    private void initDataBinding() {
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel
     }
 }
---
+++ app/src/main/java/stefanirndorfer/at/mvvm_playground/viewmodel/ActivityMainViewModel.java
@@ -0,0 +1,4 @@
+package stefanirndorfer.at.mvvm_playground.viewmodel;
+
+public class ActivityMainViewModel {
+}
